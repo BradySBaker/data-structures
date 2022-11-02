ec6f14cd9ba1d263f96a5f7e49bc1738733963b4
BradySBaker <bradysamuelbaker@gmail.com>
2022-11-02 14:50:53 -0700

    Complete sprint one

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,33 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.length = 0;
+  }
+
+  size() {
+    return this.length;
+  }
+
+  enqueue(value) {
+    var previous = this[0];
+    for (var i = 0; i <= this.length; i++) {
+      var cur = this[i];
+      this[i] = previous;
+      previous = cur;
+    }
+    this[0] = value;
+    this.length++;
+    return value;
+  }
+
+  dequeue() {
+    if (this.length === 0) {
+      return;
+    }
+    var lastIdx = this.length - 1;
+    var removed = this[lastIdx];
+    delete this[lastIdx];
+    this.length--;
+    return removed;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,26 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.length = 0;
   }
 
+  size() {
+    return this.length;
+  }
+
+  push(value) {
+    this[this.length] = value;
+    this.length++;
+    return value;
+  }
+
+  pop() {
+    if (this.length === 0) {
+      return;
+    }
+    var lastIdx = this.length - 1;
+    var removed = this[lastIdx];
+    delete this[lastIdx];
+    this.length--;
+    return removed;
+  }
 }
